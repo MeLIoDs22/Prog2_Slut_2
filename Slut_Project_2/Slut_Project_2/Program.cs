1222a72e2645df4d8c2631d715cd5860b8c5734a
MeLIoDs22 <73400357+MeLIoDs22@users.noreply.github.com>
2021-03-15 09:00:10 +0100

    Start_2
    Created some folder for organization.

--- Slut_Project_2/Slut_Project_2/Program.cs
+++ Slut_Project_2/Slut_Project_2/Program.cs
@@ -7,7 +7,7 @@ namespace Slut_Project_2
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new Main())
                 game.Run();
         }
     }
---
+++ Slut_Project_2/Slut_Project_2/Scripts/WorldManager.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Slut_Project_2.Scripts
+{
+    class WorldManager
+    {
+    }
+}
